0168bf06250f65e969a1658ee1f89f5dc2bc1b78
Felix Ferdinand <felixf020401@gmail.com>
2023-07-25 08:49:45 +0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -286,7 +286,7 @@ export default function Home() {
           </div>
         </section>
 
-        <p className='text-slate-500 text-sm mb-6 lg:mb-0 sm:mx-10 lg:ml-5'>Coded in <span className='text-slate-400'>Visual Studio Code</span> by <span className='text-slate-400'>Felix Ferdinand</span>. Built with <span className='text-slate-400'>Next.js</span> and <span className='text-slate-400'>Tailwind CSS</span>, deployed with <span className='text-slate-400'>Vercel</span>. Last modified on July 22, 2023.</p>
+        <p className='text-slate-500 text-sm mb-6 lg:mb-0 sm:mx-10 lg:ml-5'>Coded in <span className='text-slate-400'>Visual Studio Code</span> by <span className='text-slate-400'>Felix Ferdinand</span>. Built with <span className='text-slate-400'>Next.js</span> and <span className='text-slate-400'>Tailwind CSS</span>, deployed with <span className='text-slate-400'>Vercel</span>. Last modified on July 25, 2023.</p>
 
         <Modal1 open={open1} onClose={() => setOpen1(false)}>
           <Carousel1>
